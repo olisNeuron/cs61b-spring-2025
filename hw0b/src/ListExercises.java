1bd06413726c19c79fbfd0a34d959ce4bf945948
liangshuo <3980763352@qq.com>
2026-06-28 14:59:00 +0800

    hw0b: completed ListExercises.java

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
@@ -5,25 +6,52 @@ public class ListExercises {
     /** Returns the total sum in a list of integers */
     public static int sum(List<Integer> L) {
         // TODO: Fill in this function.
-        return 0;
+        int total = 0;
+        for (int i = 0;i < L.size();i++) {
+            total += L.get(i);
+        }
+        return total;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
         // TODO: Fill in this function.
-        return null;
+        ArrayList<Integer> evenList = new ArrayList<>();
+        for (int i = 0;i < L.size();i++) {
+            if (L.get(i) % 2 == 0) {
+                evenList.add(L.get(i));
+            }
+        }
+        return evenList;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
         // TODO: Fill in this function.
-        return null;
+        ArrayList<Integer> commonList = new ArrayList<>();
+        for (int i = 0;i < L1.size();i++) {
+            for (int j = 0;j < L2.size();j++) {
+                if (L1.get(i) == L2.get(j)) {
+                    commonList.add(L1.get(i));
+                }
+            }
+        }
+        return commonList;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
         // TODO: Fill in this function.
-        return 0;
+        int count = 0;
+        for (int i = 0;i < words.size();i++) {
+            String currentString = words.get(i);
+            for (int j = 0;j < currentString.length();j++) {
+                if (currentString.charAt(j) == c) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
