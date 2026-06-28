821b4115ee235f0edf33421f712ab23d312b2cc7
liangshuo <3980763352@qq.com>
2026-06-28 14:27:44 +0800

    hw0b: completed JavaExercises.java

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -1,12 +1,17 @@
+import edu.princeton.cs.algs4.In;
+import org.apache.commons.collections.list.TreeList;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 public class JavaExercises {
 
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
         // TODO: Fill in this function.
-        return null;
+        int[] array = {1, 2, 3, 4, 5, 6};
+        return array;
     }
 
     /** Returns the order depending on the customer.
@@ -15,14 +20,26 @@ public class JavaExercises {
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
         // TODO: Fill in this function.
-        return null;
+        if (customer == "Ergun") {
+            return new String[]{"beyti", "pizza", "hamburger", "tea"};
+        }else if (customer == "Erik") {
+            return new String[]{"sushi", "pasta", "avocado", "coffee"};
+        }
+        return new String[]{"","",""};
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
         // TODO: Fill in this function.
-        return 0;
+        if (array.length == 0) {
+            throw new IllegalArgumentException("array must be nonempty");
+        }
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int i = 0;i < array.length;i++){
+            set.add(array[i]);
+        }
+        return set.last() - set.first();
     }
 
     /**
@@ -39,7 +56,18 @@ public class JavaExercises {
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
         // TODO: Fill in this function.
-        return null;
+        if (x == 1) {
+            list.add(x);
+            return list;
+        }
+        else {
+            if (x % 2 == 0) {
+                list.add(x);
+                return hailstoneHelper(x / 2, list);
+            }else {
+                list.add(x);
+                return hailstoneHelper(x * 3 + 1, list);
+            }
+        }
     }
-
 }
