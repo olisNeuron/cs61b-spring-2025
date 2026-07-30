544776c84435ac03669d7622085f12438a4c1c90
liangshuo <3980763352@qq.com>
2026-07-30 14:31:09 +0800

    lec15: completed two methods of Disjointsets

--- cs61b-examples/src/lec15/QuickFindDS.java
+++ cs61b-examples/src/lec15/QuickFindDS.java
@@ -1,13 +1,29 @@
 package lec15;
 
 public class QuickFindDS implements Disjointsets{
+    int[] id;
+
+    public QuickFindDS(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i+=1) {
+            id[i] = -1;
+        }
+    }
+
     @Override
     public void connect(int q, int p) {
+        int pid = id[p];
+        int qid = id[q];
 
+        for (int i = 0; i < id.length; i+=1) {
+            if (id[i] == pid) {
+                id[i] = qid;
+            }
+        }
     }
 
     @Override
     public boolean isConnected(int q, int p) {
-        return false;
+        return id[q] == id[p];
     }
 }
