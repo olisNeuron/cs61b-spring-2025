e6e1ce95ed2edd2416f780aeead4b30a1a750766
liangshuo <3980763352@qq.com>
2026-07-31 11:53:12 +0800

    lec15: completed skeleton code of QuickUnionDS

--- cs61b-examples/src/lec15/QuickUnionDS.java
+++ cs61b-examples/src/lec15/QuickUnionDS.java
@@ -1,9 +1,27 @@
 package lec15;
 
 public class QuickUnionDS implements Disjointsets{
+    private int[] parent;
+
+    public QuickUnionDS(int N) {
+        parent = new int[N];
+        for (int i = 0; i < N; i+=1) {
+            parent[i] = -1;
+        }
+    }
+
+    public int find(int p) {
+        int r = p;
+        while (parent[r] >= 0) {
+            r = parent[r];
+        }
+        return r;
+    }
 
     @Override
     public void connect(int q, int p) {
+        int i = find(q);
+        int j = find(p);
 
     }
 
