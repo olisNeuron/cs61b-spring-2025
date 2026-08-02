45504dc21268640d31d81a0db6aea144244a1535
liangshuo <3980763352@qq.com>
2026-08-02 10:47:58 +0800

    hw2: implement O(N) version of isFull

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -1,58 +1,71 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import org.apache.bcel.generic.ATHROW;
 
 public class Percolation {
     // TODO: Add any necessary instance variables.
-    private List<Integer> openBlock;
-    private int[][] grid;
-
-    //vitual blocks that point to bottom and top of the grid element
-    private int topBlock = 9999;
-    private int bottomBlock = 999;
+    public boolean[][] siteArray;
+    public WeightedQuickUnionUF uf;
 
     public Percolation(int N) {
         // TODO: Fill in this constructor.
-        // list of open block
-        openBlock = new ArrayList<>();
+        siteArray = new boolean[N][N];
+        uf = new WeightedQuickUnionUF(N * N);
+    }
 
-        //construct N*N grid
-        grid = new int[N][N];
-        int totalNum = 0;
-        for (int i = 0; i < N; i+=1) {
-            for (int j = 0; j < N; j+=1) {
-                grid[i][j] = totalNum;
-                totalNum += 1;
+    public int transform(int row, int col) {
+        return row*(siteArray.length)+col;
+    }
+
+    public void connectNeighbor(int row, int col) {
+        int[][] checkArray = {{0, 1},{1, 0},{-1, 0},{0, -1}};
+        for (int[] ca : checkArray) {
+            int r = row-ca[0];
+            int c = col-ca[1];
+
+            if (isOpen(r, c)) {
+                uf.union(
+                        transform(row, col),
+                        transform(r, c)
+                );
             }
-        }
-
-        //make topBlock and bottomBlock pointing to the top row and bottom row
-        for (int i = 0; i < N; i+=1) {
-            
         }
     }
 
     public void open(int row, int col) {
         // TODO: Fill in this method.
-        if (!openBlock.contains(grid[row][col])){
-            openBlock.add(grid[row][col]);
-        }
+        siteArray[row][col] = true;
+        connectNeighbor(row, col);
     }
 
     public boolean isOpen(int row, int col) {
         // TODO: Fill in this method.
-        return openBlock.contains(grid[row][col]);
+        if (row < 0 || row >= siteArray.length || col < 0 || col >= siteArray.length) {
+            return false;
+        }
+        return siteArray[row][col];
     }
 
     public boolean isFull(int row, int col) {
         // TODO: Fill in this method.
+        if (!isOpen(row, col)) {
+            return false;
+        }
+        for (int i = 0; i < siteArray.length; i++) {
+            if (uf.connected(transform(0, i), transform(row, col))) {
+                return true;
+            }
+        }
         return false;
     }
 
     public int numberOfOpenSites() {
-        // TODO: Fill in this method.
+//        // TODO: Fill in this method.
+//        int totalNum = 0;
+//        for (boolean[] sa : siteArray) {
+//            for (boolean saInside : sa) {
+//                totalNum += 1;
+//            }
+//        }
         return 0;
     }
 
