3c1082f9cb556cb8be0328369a8e6b84306a77d3
liangshuo <3980763352@qq.com>
2026-09-05 11:18:59 +0800

    proj2b: add proj2a implementation into Ngrams

---
+++ skeleton-code-sp25/proj2b/src/ngrams/proj2a/src/main/Main.java
@@ -0,0 +1,29 @@
+package ngrams.proj2a.src.main;
+
+import main.HistoryHandler;
+import main.HistoryTextHandler;
+import ngrams.NGramMap;
+import org.slf4j.LoggerFactory;
+import browser.NgordnetServer;
+
+import static utils.Utils.TOP_49887_WORDS_FILE;
+import static utils.Utils.TOTAL_COUNTS_FILE;
+
+public class Main {
+    static {
+        LoggerFactory.getLogger(Main.class).info("\033[1;38mChanging text color to white");
+    }
+    /* Do not delete or modify the static code above! */
+
+    public static void main(String[] args) {
+        NgordnetServer hns = new NgordnetServer();
+
+        NGramMap ngm = new NGramMap(TOP_49887_WORDS_FILE, TOTAL_COUNTS_FILE);
+
+        hns.startUp();
+        hns.register("history", new HistoryHandler(ngm));
+        hns.register("historytext", new HistoryTextHandler(ngm));
+
+        System.out.println("Finished server startup! Visit http://localhost:4567/ngordnet_2a.html");
+    }
+}
